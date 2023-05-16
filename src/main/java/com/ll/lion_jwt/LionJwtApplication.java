23c39cf52eb9053ccf58d58e08d0cd83f6012753
Youngwoo Choi <yw823x@daum.net>
2023-05-16 14:37:19 +0900

    Initial Commit

---
+++ src/main/java/com/ll/lion_jwt/LionJwtApplication.java
@@ -0,0 +1,13 @@
+package com.ll.lion_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LionJwtApplication.class, args);
+    }
+
+}
