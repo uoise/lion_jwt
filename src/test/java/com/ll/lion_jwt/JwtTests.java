6cc1745d9c43494599e97c2276f00a4c48e83ef7
Youngwoo Choi <yw823x@daum.net>
2023-05-16 15:20:37 +0900

    jwt provider

--- src/test/java/com/ll/lion_jwt/JwtTests.java
+++ src/test/java/com/ll/lion_jwt/JwtTests.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -53,5 +55,22 @@ class JwtTests {
         SecretKey secretKey2 = jwtProvider.getSecretKey();
 
         assertThat(secretKey1).isEqualTo(secretKey2);
+    }
+
+
+    @Test
+    @DisplayName("accessToken 을 얻는다.")
+    void t5() {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", 1L);
+        claims.put("username", "admin");
+        claims.put("steve", "jobs");
+
+        // 지금으로부터 5시간의 유효기간을 가지는 토큰을 생성
+        String accessToken = jwtProvider.genToken(claims, 60 * 60 * 5);
+
+        System.out.println("accessToken : " + accessToken);
+
+        assertThat(accessToken).isNotNull();
     }
 }
